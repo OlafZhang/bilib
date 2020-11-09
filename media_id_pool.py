4629322b137f4083f7a9bf60a6bd2913a42a56b3
OlafZhang <55549042+OlafZhang@users.noreply.github.com>
2020-11-09 11:03:52 +0800

    单独的mediaID池
    单独的mediaID池

---
+++ media_id_pool.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+#coding:utf-8
+# 存储一些番剧/电影的mediaID池
+
+# 轻音少女(大会员)
+# 剧场版无法返回清晰度信息，需要带上cookie，考虑写
+kon_1 = 28220978
+kon_2 = 28220984
+kon_movie = 28220988
+
+# 玉子市场(免费，第二个是剧场版)
+tamako_market = 116772
+tamako_love_story = 4155
+
+# JOJO的奇妙冒险(大会员)
+jojo_1_2 = 28223479
+jojo_3_ep1 = 28223481
+jojo_3_ep2 = 28223483
+jojo_4 = 140552
+jojo_5 = 135652
+
+# Love Live(免费，包含Sunshine)
+ll_1 = 3068
+ll_2 = 3071
+llss_1 = 5062
+llss_2 = 6433
+
+# 笨女孩(免费，加长版的泡面番)
+aho_girl = 6311
+
+# 吹响吧！上低音号(京吹)
+# 第一季免费，第二季大会员
+enphonium_1 = 1547
+enphonium_2 = 28223434
+
+# 工作细胞(大会员)
+work_cell_1 = 102392
+
+# 鬼灭之刃(大会员)
+kimetsu_no_yaiba = 22718131
+
+# 辉夜大小姐想让我告白(大会员，分集名称有转义字符)
+kaguya_1 = 5267730
+kaguya_2 = 28228367
+
+# 天使降临到我身边(大会员)
+tenshi = 4316442
+
+# 干物妹！小埋(大会员)
+umr_1 = 2580
+umr_2 = 6446
+
+# 小林家的龙女仆(大会员)
+maid_dragon_1 = 5800
+
+# 日常(大会员)
+nichijou = 844
+
+# 擅长捉弄的高木同学 第二季(大会员，分集名称有转义字符)
+takagi_2 = 28221403
+
+# 冰菓(大会员，有11.5集)
+hyouka = 3398
+
+# 境界的彼方(大会员)
+beyond_the_boundary = 3365
+
+# 中二病也要谈恋爱(大会员)
+chunibyo_1 = 4340
+chunibyo_2 = 4349
+chunibyo_movie = 115632
+
+# 女高中生的虚度日常(大会员)
+high_school_girls = 28221415
+
+# 公主连结RE:DIVE(大会员，重命名带番剧名称时注意半角冒号)
+princess_connect = 28228339
+
+# 天气之子(大会员，且因为此电影曾触发了大量lib中错误所以决定保留在demo)
+weathering_with_you = 28228734
+
+# 猫和老鼠旧版(免费，最大画质480P)
+tom_and_jerry = 132112
+
+# 环太平洋(大会员)
+pacific_rim = 28227720
