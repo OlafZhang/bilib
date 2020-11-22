14d6b7126914cd9827655e6c720a1782e0d8f11f
OlafZhang <55549042+OlafZhang@users.noreply.github.com>
2020-11-22 18:27:30 +0800

    PyCharm重新格式化

--- bilib.py
+++ bilib.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-#coding:utf-8
+# coding:utf-8
 
+import csv
 # 这是一个lib，引用大量B站API，目前用户信息和弹幕工作正常，其它存在潜在的bug
 # bilib = bili + lib
 import os
-import csv
 import re
 import sys
 import time
@@ -18,6 +18,7 @@ try:
     from fake_useragent import UserAgent
 except:
     import platform
+
     sysstr = platform.system()
     if sysstr == "Windows":
         os.system("pip install -r requirements.txt")
@@ -28,33 +29,39 @@ except:
     from fake_useragent import UserAgent
 
 
-
 # 传参异常/未定义异常
 class InfoError(Exception):
     pass
+
 
 # 弹幕文件相关异常
 class danmakuError(Exception):
     pass
 
+
 # 服务调用超时
 class Timeout(Exception):
     pass
+
 
 # 请求错误
 class RequestError(Exception):
     pass
 
+
 # 啥都木有
 class SeemsNothing(Exception):
     pass
+
 
 # 请求被拦截
 class RequestRefuse(Exception):
     pass
 
+
 # 默认超时时间为5秒，应用到所有requests.get()
 timeout = 5
+
 
 # 设置超时时间
 def set_timeout(set_time):
@@ -64,16 +71,18 @@ def set_timeout(set_time):
     except:
         timeout = 5
 
+
 # 默认没有cookie，应用到所有requests.get()
 # 由于带入cookie有些番剧还是不能得到正确消息，故不做cookie模块
 cookies = ""
 with_cookies = False
 
+
 # 获取视频最高清晰度
 # 传入参数：av号,bv号或ep号
 # 由于同时支持视频和番剧，故单独做了API，作为备份
 # 已集成到video_info()，anime_base_info()则使用了另外的方法
-def get_resolution(id_input,getid = False):
+def get_resolution(id_input, getid=False):
     id_input = str(id_input)
     # 判断用户输入的是av号还是bv号
     if str(id_input[0:2]).isalpha():
@@ -94,7 +103,7 @@ def get_resolution(id_input,getid = False):
             main_url = str("https://www.bilibili.com/bangumi/play/" + str(id_input))
         else:
             main_url = str("https://www.bilibili.com/video/" + str(id_input))
-        main_url = requests.get(main_url, headers=headers, timeout=timeout, cookies = cookies)
+        main_url = requests.get(main_url, headers=headers, timeout=timeout, cookies=cookies)
     except:
         raise Timeout("Timeout.")
     quality = str("不支持")
@@ -145,7 +154,6 @@ def get_resolution(id_input,getid = False):
         return quality
 
 
-
 # 获取视频信息
 # 对于番剧/电影，除了能配合anime_episode_info获得bv号、视频原生分辨率以外，没有任何作用，且数据比较不可信
 # 而av号和bv号对番剧/电影来说没有意义
@@ -168,9 +176,11 @@ def video_info(id_input):
     # 返回的结果全部来自此API
     try:
         if mode == "bv":
-            play_info = requests.get("https://api.bilibili.com/x/web-interface/view?bvid=" + str(id_input), headers=headers,timeout=timeout, cookies = cookies)
+            play_info = requests.get("https://api.bilibili.com/x/web-interface/view?bvid=" + str(id_input),
+                                     headers=headers, timeout=timeout, cookies=cookies)
         elif mode == "av":
-            play_info = requests.get("https://api.bilibili.com/x/web-interface/view?aid=" + str(id_input), headers=headers,timeout=timeout, cookies = cookies)
+            play_info = requests.get("https://api.bilibili.com/x/web-interface/view?aid=" + str(id_input),
+                                     headers=headers, timeout=timeout, cookies=cookies)
         else:
             raise InfoError("You should input 'av' or 'bv'.")
     except requests.exceptions.ReadTimeout:
@@ -194,7 +204,7 @@ def video_info(id_input):
         headers = {"User-Agent": ua}
         try:
             main_url = str("https://www.bilibili.com/video/" + str(bvid))
-            main_url = requests.get(main_url,headers = headers,timeout = timeout, cookies = cookies)
+            main_url = requests.get(main_url, headers=headers, timeout=timeout, cookies=cookies)
         except:
             raise Timeout("Timeout.")
         soup = BeautifulSoup(main_url.text, "html.parser")
@@ -241,7 +251,7 @@ def video_info(id_input):
 
         return_dict = {"aid": aid, "bvid": bvid, "cid": cid, "title": title, "desc": desc, "owner_name": owner_name,
                        "owner_uid": owner_uid, "view": view, "danmaku": danmaku, "reply": reply, "favorite": favorite,
-                       "coin": coin, "share": share, "like": like, "quality": quality,"quality_id": quality_id}
+                       "coin": coin, "share": share, "like": like, "quality": quality, "quality_id": quality_id}
         # 返回字典，总共使用1个API和一个HTML页
         return return_dict
     except:
@@ -260,7 +270,6 @@ def video_info(id_input):
             raise InfoError("Something error.")
 
 
-
 # 番剧/电影剧集信息（返回每集的cid，封面URL等，cid可用于爬取弹幕）
 def anime_episode_info(season_id):
     ua = str(UserAgent(use_cache_server=False).random)
@@ -269,7 +278,7 @@ def anime_episode_info(season_id):
     # 返回的结果全部来自此API
     try:
         play_info = requests.get("https://api.bilibili.com/pgc/web/season/section?season_id=" + str(id_input),
-                                 headers=headers,timeout=timeout, cookies = cookies)
+                                 headers=headers, timeout=timeout, cookies=cookies)
         play_info = play_info.json()
     except requests.exceptions.ReadTimeout:
         raise Timeout("Timeout.")
@@ -353,7 +362,6 @@ def anime_episode_info(season_id):
     return return_dict
 
 
-
 # 番剧/电影基本信息（返回seasonID,以便请求剧集的详情）
 def anime_base_info(media_id):
     ua = str(UserAgent(use_cache_server=False).random)
@@ -361,7 +369,8 @@ def anime_base_info(media_id):
     headers = {"Host": "api.bilibili.com", "User-Agent": ua}
     # 返回的结果基本来自此API
     try:
-        play_info = requests.get("https://api.bilibili.com/pgc/review/user?media_id=" + str(id_input), headers=headers,timeout=timeout, cookies = cookies)
+        play_info = requests.get("https://api.bilibili.com/pgc/review/user?media_id=" + str(id_input), headers=headers,
+                                 timeout=timeout, cookies=cookies)
     except requests.exceptions.ReadTimeout:
         raise Timeout("Timeout.")
     play_info = play_info.json()
@@ -390,7 +399,7 @@ def anime_base_info(media_id):
         # 获取tagID，此参数可以获得番剧/电影的相关推荐，如其它相关番剧和二创
         try:
             tag_info = requests.get("https://api.bilibili.com/x/tag/info?tag_name=" + str(title),
-                                    headers=headers,timeout=timeout, cookies = cookies)
+                                    headers=headers, timeout=timeout, cookies=cookies)
         except requests.exceptions.ReadTimeout:
             raise Timeout("Timeout.")
         try:
@@ -409,8 +418,9 @@ def anime_base_info(media_id):
         bv_no = video_info(av_no)["bvid"]
         cid = str(episode_info[key]["cid"])
         try:
-            quality_info = requests.get("https://api.bilibili.com/pgc/player/web/playurl?aid=" + str(av_no) + "&cid=" + str(cid),
-                                    headers=headers, timeout=timeout, cookies = cookies)
+            quality_info = requests.get(
+                "https://api.bilibili.com/pgc/player/web/playurl?aid=" + str(av_no) + "&cid=" + str(cid),
+                headers=headers, timeout=timeout, cookies=cookies)
         except requests.exceptions.ReadTimeout:
             raise Timeout("Timeout.")
 
@@ -427,7 +437,7 @@ def anime_base_info(media_id):
         # 根据seasonID求总投币数，追番数等信息
         try:
             other_info = requests.get("https://api.bilibili.com/pgc/web/season/stat?season_id=" + str(season_id),
-                                      headers=headers,timeout=timeout, cookies = cookies)
+                                      headers=headers, timeout=timeout, cookies=cookies)
         except requests.exceptions.ReadTimeout:
             raise Timeout("Timeout.")
         other_info = other_info.json()
@@ -439,7 +449,8 @@ def anime_base_info(media_id):
         try:
             # 获取HTML页，检查介绍信息和是否收费观看
             headers = {"User-Agent": ua}
-            url = requests.get("https://www.bilibili.com/bangumi/media/md%s" % str(media_id), headers=headers,timeout=timeout, cookies = cookies)
+            url = requests.get("https://www.bilibili.com/bangumi/media/md%s" % str(media_id), headers=headers,
+                               timeout=timeout, cookies=cookies)
         except requests.exceptions.ReadTimeout:
             raise Timeout("Timeout.")
         soup = BeautifulSoup(url.text, "html.parser")
@@ -491,7 +502,7 @@ def anime_base_info(media_id):
                         vip_info = vip_info.replace("　", "")
                     except IndexError:
                         # 均未匹配则表示免费
-                         vip_info = str("免费")
+                        vip_info = str("免费")
                 if str("开通大会员观看") == str(vip_info):
                     vip_info = str("大会员")
                 elif str("成为大会员免费看") == str(vip_info):
@@ -612,10 +623,11 @@ def anime_base_info(media_id):
                        "cover_url": cover_url, "media_id": media_id, "ep_id": ep_id, "episode": episode,
                        "rating_count": rating_count, "score": score, "season_id": season_id, "coins": coins,
                        "danmakus": danmakus, "follow": follow, "series_follow": series_follow, "views": views,
-                       "tag_id": tag_id, "vip_info": vip_info, "aid" : av_no,"bvid": bv_no,"quality":quality,
-                       "quality_ID":quality_ID,"is_finish":is_finish,"is_started":is_started,"actor_list":actor_list,
-                       "staff_list":staff_list,"flag_list":flag_list,"alias_list":alias_list,"showtime":showtime,
-                       "origin_name":origin_name}
+                       "tag_id": tag_id, "vip_info": vip_info, "aid": av_no, "bvid": bv_no, "quality": quality,
+                       "quality_ID": quality_ID, "is_finish": is_finish, "is_started": is_started,
+                       "actor_list": actor_list,
+                       "staff_list": staff_list, "flag_list": flag_list, "alias_list": alias_list, "showtime": showtime,
+                       "origin_name": origin_name}
         if return_dict:
             return return_dict
         else:
@@ -640,7 +652,6 @@ def anime_base_info(media_id):
             raise InfoError("Something error.")
 
 
-
 # 获取用户信息
 # 这个是实验性API，理论效果更好，功能更多，流量开销更小
 def user_info(uid_input):
@@ -649,7 +660,8 @@ def user_info(uid_input):
     headers = {"Host": "api.bilibili.com", "User-Agent": ua}
     # 返回的结果基本来自此API
     try:
-        info_get = requests.get("https://api.bilibili.com/x/space/acc/info?mid=" + str(uid_input), headers=headers,timeout=timeout, cookies = cookies)
+        info_get = requests.get("https://api.bilibili.com/x/space/acc/info?mid=" + str(uid_input), headers=headers,
+                                timeout=timeout, cookies=cookies)
     except requests.exceptions.ReadTimeout:
         raise Timeout("Timeout.")
     info_get = info_get.json()
@@ -687,7 +699,8 @@ def user_info(uid_input):
         vip_type == str("None")
     try:
         # 获取关注/粉丝量
-        fans = requests.get("https://api.bilibili.com/x/relation/stat?vmid=" + str(uid_input), headers=headers,timeout=timeout, cookies = cookies)
+        fans = requests.get("https://api.bilibili.com/x/relation/stat?vmid=" + str(uid_input), headers=headers,
+                            timeout=timeout, cookies=cookies)
     except requests.exceptions.ReadTimeout:
         raise Timeout("Timeout.")
     fans = fans.json()
@@ -699,7 +712,6 @@ def user_info(uid_input):
     return return_dict
 
 
-
 # 获取用户信息 旧版
 # 从作者的爬取项目独立开的API，使用了一个API和一个HTML页，但功能较少，现只做保留，请自行研究
 def user_info_old(uid_input, get_ua=False):
@@ -707,7 +719,8 @@ def user_info_old(uid_input, get_ua=False):
     ua = str(UserAgent(use_cache_server=False).random)
     headers = {"User-Agent": ua}
     try:
-        name = requests.get("https://space.bilibili.com/" + str(uid_input), headers=headers,timeout=timeout, cookies = cookies)
+        name = requests.get("https://space.bilibili.com/" + str(uid_input), headers=headers, timeout=timeout,
+                            cookies=cookies)
     except requests.exceptions.ReadTimeout:
         raise Timeout("Timeout.")
     headers = {"Host": "api.bilibili.com", "User-Agent": ua}
@@ -722,7 +735,8 @@ def user_info_old(uid_input, get_ua=False):
         connect_ok = True
     try:
         try:
-            fans = requests.get("https://api.bilibili.com/x/relation/stat?vmid=" + str(uid_input), headers=headers,timeout=timeout)
+            fans = requests.get("https://api.bilibili.com/x/relation/stat?vmid=" + str(uid_input), headers=headers,
+                                timeout=timeout)
         except requests.exceptions.ReadTimeout:
             raise Timeout("Timeout.")
         fans = fans.json()
@@ -750,7 +764,6 @@ def user_info_old(uid_input, get_ua=False):
     return return_dict
 
 
-
 # 获取弹幕(自动转换为可读性较高的csv)
 def get_danmaku(cid_input, reset=False):
     try:
@@ -772,7 +785,7 @@ def get_danmaku(cid_input, reset=False):
         bvIndex = url.find('BV')
         id = url[bvIndex:]
         try:
-            rr = requests.get(url=url,timeout=timeout, cookies = cookies)
+            rr = requests.get(url=url, timeout=timeout, cookies=cookies)
         except requests.exceptions.ReadTimeout:
             raise Timeout("Timeout.")
         rr.encoding = 'uft-16'
@@ -856,7 +869,6 @@ def get_danmaku(cid_input, reset=False):
 
     except Exception as e:
         print(e)
-
 
 
 # 根据csv文件列出所有弹幕
@@ -983,7 +995,6 @@ def listall_danmaku(file_path, stamp=False):
         return return_thing
 
 
-
 # 数弹幕行数
 # 虽然没什么难的，但是意义在于辅助弹幕文件的遍历
 def count_danmaku(file_path):
@@ -994,7 +1005,6 @@ def count_danmaku(file_path):
 
     file_path = open(str(file_path), 'r', encoding='utf-16')
     return len(file_path.readlines())
-
 
 
 # 获取弹幕(原始信息，即xml)
@@ -1019,7 +1029,7 @@ def get_danmaku_raw(cid_input, reset=False):
 
         url = str('http://comment.bilibili.com/' + str(cid_input) + '.xml')
         try:
-            rr = requests.get(url=url, timeout=timeout, cookies = cookies)
+            rr = requests.get(url=url, timeout=timeout, cookies=cookies)
         except requests.exceptions.ReadTimeout:
             raise Timeout("Timeout.")
         rr.encoding = 'uft-8'
@@ -1033,7 +1043,6 @@ def get_danmaku_raw(cid_input, reset=False):
         print(e)
 
 
-
 # 弹幕文件(xml)转字幕文件(ass)
 # 对于某些不支持弹幕的播放器可以用这个API
 # 现支持跨平台运行
@@ -1043,7 +1052,7 @@ def raw2ass(file_path):
     sysstr = platform.system()
     final_file = str(str(file_path).split('.xml')[0]) + ".ass"
     if sysstr == "Windows":
-        os.system("python .\\niconvert-master\\main.py \"" + str(file_path) + "\" -o \"" + str(final_file)+ "\"")
+        os.system("python .\\niconvert-master\\main.py \"" + str(file_path) + "\" -o \"" + str(final_file) + "\"")
     else:
         os.system("python3 ./niconvert-master/main.py \"" + str(file_path) + "\" -o \"" + str(final_file) + "\"")
     for i in range(0, 60):
@@ -1056,7 +1065,6 @@ def raw2ass(file_path):
     print("FAIL")
 
 
-
 # 搜索番剧(beta)
 # 目前只支持搜索番剧，输入关键词返回字典，含有md号
 # 最多返回20条
@@ -1065,7 +1073,8 @@ def search_anime(keyword):
     ua = str(UserAgent(use_cache_server=False).random)
     headers = {"User-Agent": ua}
     # 搜索，拿到season_id
-    search_info = requests.get("https://search.bilibili.com/bangumi?keyword=" + str(keyword),headers=headers, timeout=timeout, cookies = cookies)
+    search_info = requests.get("https://search.bilibili.com/bangumi?keyword=" + str(keyword), headers=headers,
+                               timeout=timeout, cookies=cookies)
     if str(search_info.status_code) == str("404"):
         return return_dict
     elif str(search_info.status_code) == str("412"):
@@ -1077,7 +1086,7 @@ def search_anime(keyword):
         if str("window.__INITIAL_STATE__=") in str(x.string):
             text = str(x.string)
             # 由于不稳定，将bs4输出注释以方便调试
-            #print(text)
+            # print(text)
             break
         else:
             pass
@@ -1094,14 +1103,22 @@ def search_anime(keyword):
         try:
             season_id = re.findall("ss\d+", string)[0]
             try:
-                #在爬取eva的时候出现了奇怪的bug，class=keyword关键字不止一个
-                string = string.split(r'class="keyword"')[2]
+                # 在爬取eva的时候出现了奇怪的bug，class=keyword关键字不止一个
+                stringA = string.split(r'class="keyword"')[1]
+                stringA = stringA.split(r'","org_title":"')[0]
+                stringA = stringA.split(r'"')[0]
+                stringB = string.split(r'class="keyword"')[2]
+                stringB = stringB.split(r'","org_title":"')[0]
+                stringB = stringB.split(r'"')[0]
+                string = stringA + str(" ") + stringB
             except:
                 string = string.split(r'class="keyword"')[1]
-            string = string.split(r'","org_title":"')[0]
-            string = string.split(r'"')[0]
+                string = string.split(r'","org_title":"')[0]
+                string = string.split(r'"')[0]
+
             # 跳转到播放页，拿到season_id
-            md_info = requests.get("https://www.bilibili.com/bangumi/play/" + str(season_id),headers=headers, timeout=timeout, cookies = cookies)
+            md_info = requests.get("https://www.bilibili.com/bangumi/play/" + str(season_id), headers=headers,
+                                   timeout=timeout, cookies=cookies)
             md_info = md_info.text
             media_id = re.findall("md\d+", md_info)[0]
             anime = string
