1aad2067ba293e1f9712270ce44734aa9431b40a
OlafZhang <55549042+OlafZhang@users.noreply.github.com>
2021-01-04 22:15:08 +0800

    更新番剧列表
    + 总之就是非常可爱
    + 幸运星
    + 爱杀宝贝
    + 魔法禁书目录 系列
    + 某科学的超电磁炮 系列
    + CLANNAD 系列
    + 夏目友人帐 系列
    + LoveLive！ 系列
    + 吹响吧！上低音号 剧场版
    + 境界的彼方 剧场版
    + 凉宫春日的忧郁 系列

--- media_id_pool.py
+++ media_id_pool.py
@@ -18,40 +18,72 @@ jojo_3_ep2 = 28223483
 jojo_4 = 140552
 jojo_5 = 135652
 
+# 总之就是非常可爱(大会员，截止到20200101限免)
+# 本人一定要吐槽一下这部番的英文名，没看过根本想不到(还是得拿来做变量名)
+fly_me_to_moon = 28229676
+
 # 斩服少女(大会员)
 kill_la_kill = 419
 
+# 幸运星(免费)
+lucky_star = 1293
+
+# 爱杀宝贝(免费)，B站妥协使用了"啾咪BABY"这一名称
+kill_me_baby = 709
+
+# 魔法禁书目录(第三季大会员)
+magical_index_1 = 963
+magical_index_2 = 964
+magical_index_3 = 134912
+
 # 某科学的超电磁炮(大会员)
 railgun_1 = 425
+railgun_2 = 427
+railgun_3 = 28224095
 
 # 未闻花名(大会员)
 ano_hana = 835
 
 # CLANNAD(免费)
-clannad = 1177
+clannad_1 = 1177
+clannad_2 = 1178
 
 # EVA(免费)
+# 本人非eva厨且不熟悉，所以不放剧场版
 eva = 1635
 
 # 夏目友人帐(大会员)
-natsume = 1660
+natsume_1 = 1660
+natsume_2 = 1661
+natsume_3 = 1662
+natsume_4 = 1663
+natsume_5 = 5550
+natsume_6 = 5977
+natsume_movie = 28221382
 
-# Love Live(免费，包含Sunshine)
+# Love Live(免费，包含Sunshine和虹咲学园)
 ll_1 = 3068
 ll_2 = 3071
 llss_1 = 5062
 llss_2 = 6433
+ll_3_1 = 282298
 ll_movie = 28230704
+ll_ss_movie = 28228631
 
 # 笨女孩(免费)
 aho_girl = 6311
 
-# 吹响吧！上低音号(第一季免费，第二季大会员)
+# 吹响吧！上低音号(第一季免费，第二季大会员，剧场版均大会员)
 enphonium_1 = 1547
 enphonium_2 = 28223434
+enphonium_movie_1 = 28223435
+enphonium_movie_2 = 28223447
+enphonium_movie_3 = 28223448
 
 # 工作细胞(大会员)
 work_cell_1 = 102392
+#work_cell_2 = 
+#work_cell_black = 
 
 # 鬼灭之刃(大会员)
 kimetsu_no_yaiba = 22718131
@@ -59,6 +91,7 @@ kimetsu_no_yaiba = 22718131
 # 辉夜大小姐想让我告白(大会员，分集名称有转义字符)
 kaguya_1 = 5267730
 kaguya_2 = 28228367
+#kaguya_3 = 
 
 # 天使降临到我身边(大会员)
 tenshi = 4316442
@@ -69,6 +102,7 @@ umr_2 = 6446
 
 # 小林家的龙女仆(大会员)
 maid_dragon_1 = 5800
+#maid_dragon_2 = 
 
 # 日常(大会员)
 nichijou = 844
@@ -81,13 +115,17 @@ nora_2 = 2732
 suzaki_nishi = 2593
 
 # 擅长捉弄的高木同学 第二季(大会员，分集名称有转义字符)
+#takagi_1 = 
 takagi_2 = 28221403
+#takagi_3 = 
 
 # 冰菓(大会员，有11.5集)
 hyouka = 3398
 
-# 境界的彼方(大会员)
+# 境界的彼方(大会员，剧场版分别是[栗山]未来篇和过去篇)
 beyond_the_boundary = 3365
+beyond_the_boundary_mirai = 2844
+beyond_the_boundary_past = 3366
 
 # 紫罗兰永恒花园(大会员)
 # 此为B站一个例外：标明付费但大会员可以看，推测是服务端程序的问题(据说是用Go写的)
@@ -109,6 +147,14 @@ high_school_girls = 28221415
 
 # 公主连结RE:DIVE(大会员，注意半角冒号)
 princess_connect = 28228339
+
+# 凉宫春日的忧郁(大会员)
+# 分别是06版，09版，小凉宫，剧场版(凉宫春日的消失)，长门有希酱的消失
+suzumiya_2006 = 1056
+suzumiya_2009 = 24120616
+suzumiya_little = 4735
+suzumiya_movie = 2809
+nagato_yuki = 1558
 
 # 天气之子(大会员，且因为此电影曾触发了大量lib中错误所以决定保留在demo)
 weathering_with_you = 28228734
